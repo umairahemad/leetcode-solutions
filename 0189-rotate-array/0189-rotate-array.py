c6a29a95bde4789beb92c1c335d822587ab2bb48
Shaikh Umair Ahemad <umairahemad409@gmail.com>
2026-07-02 11:19:13 +0530

    Time: 7 ms (54.41%), Space: 26.7 MB (26.09%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,10 +4,10 @@ class Solution:
 
         def reverse(left, right):
             while left < right:
-                nums[left], nums[right] = nums[right], nums[left] 
+                nums[left], nums[right] = nums[right], nums[left]
                 left += 1
-                right -= 1 
-        
+                right -= 1
+
         reverse(0, len(nums) - 1)
         reverse(0, k - 1)
         reverse(k, len(nums) - 1)

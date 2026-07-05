152fe8a778c71d18106f12726613b219880e7bcb
Shaikh Umair Ahemad <umairahemad409@gmail.com>
2026-07-05 11:45:20 +0530

    Time: 6 ms (43.17%), Space: 19.2 MB (81.5%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -2,7 +2,7 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         s_to_t = {}
         t_to_s = {}
-
+        
         for i in range(len(s)):
             c1 = s[i]
             c2 = t[i]
@@ -12,8 +12,7 @@ class Solution:
             if c2 in t_to_s and t_to_s[c2] != c1:
                 return False
             
-            s_to_t[c1] = c2
+            s_to_t[c1] = c2 
             t_to_s[c2] = c1
-        return True 
 
-        
+        return True 

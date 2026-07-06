e49da272a3ab401f2379fd627fa393fdbd5b0ded
Shaikh Umair Ahemad <umairahemad409@gmail.com>
2026-07-06 17:29:07 +0530

    Time: 44 ms (65.24%), Space: 19.3 MB (16.99%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -3,14 +3,12 @@ class Solution:
         ans = []
         i = 0
         while i < len(word1) or i < len(word2):
-
             if i < len(word1):
                 ans.append(word1[i])
             if i < len(word2):
                 ans.append(word2[i])
             i += 1
         return ''.join(ans)
-                
 
         
         

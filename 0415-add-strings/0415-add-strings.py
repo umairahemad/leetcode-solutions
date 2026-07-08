9f2064f754db79a0e31fe9d8d669e862977ba780
Shaikh Umair Ahemad <umairahemad409@gmail.com>
2026-07-08 18:42:56 +0530

    Time: 4 ms (55.15%), Space: 19.4 MB (70.02%) - LeetHub

--- 0415-add-strings/0415-add-strings.py
+++ 0415-add-strings/0415-add-strings.py
@@ -1,7 +1,7 @@
 import sys
 
 # Increase the limit to 10,000 digits if needed
-sys.set_int_max_str_digits(10000)
+sys.set_int_max_str_digits(6000)
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
         return str(int(num1) + int(num2))

198a149bd3dcba78bd4fbbbebed192193ce6dbda
Shaikh Umair Ahemad <umairahemad409@gmail.com>
2026-07-09 20:48:15 +0530

    Time: 0 ms (100%), Space: 20 MB (11.03%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -5,12 +5,13 @@ class Solution:
         right = len(nums) - 1
 
         while(left <= right):
-            mid = (left + right) // 2
+            mid = (left + right) // 2 
             if nums[mid] == target:
-                return mid 
+                return mid
             elif nums[mid] < target:
                 left = mid + 1
             else:
                 right = mid - 1
         return left 
+     
         

acdf1786f3ed98a32855c410bc74ded4d7347fe6
Shaikh Umair Ahemad <umairahemad409@gmail.com>
2026-07-12 19:49:55 +0530

    Time: 0 ms (100%), Space: 19.3 MB (76.66%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -2,7 +2,6 @@ class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         nums1[:] = nums1[:m] + nums2
         return nums1.sort()
-
         # in this [:] gives whole array in the output
         # [:m] this means we want elements according to m i.e 3 so it ignores those zeros
 
